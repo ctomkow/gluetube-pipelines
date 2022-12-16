c8a4934423e64754d95a9cf8850644d0a3215734
Craig Tomkow <ctomkow@gmail.com>
2022-12-16 18:50:02 +0000

    minimal working example

--- test_pipeline/weather_data.py
+++ test_pipeline/weather_data.py
@@ -2,6 +2,18 @@
 import requests
 from jsonparse import Parser
 
-data = requests.get('https://wttr.in/Vancouver').json()
+from gluetube.pipeline import stage
 
-# todo: work in progress
+
+@stage(1, 'get weather')
+def get_weather():
+    return requests.get('https://wttr.in/Vancouver?format=j1').json()
+
+
+@stage(2, 'display weather')
+def display_avg_temps(data):
+    print(Parser().find_keys(data, ['date', 'avgtempC']))
+
+
+weather = get_weather()
+display_avg_temps(weather)
