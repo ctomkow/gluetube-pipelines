fe339438b54ad8c82b5c5f57dcf058b10976d0cc
Craig Tomkow <ctomkow@users.noreply.github.com>
2022-12-21 01:26:49 +0000

    fix

--- test_pipeline/weather_data.py
+++ test_pipeline/weather_data.py
@@ -10,10 +10,11 @@ def get_weather():
     return requests.get('https://wttr.in/Vancouver?format=j1').json()
 
 
-@stage(2, 'display weather')
-def display_avg_temps(data):
-    print(Parser().find_keys(data, ['date', 'avgtempC']))
+@stage(2, 'write weather to file')
+def write_out_avg_temps(data):
+    with open('weather.json', 'x') as f:
+        f.write(str(Parser().find_keys(data, ['date', 'avgtempC'])))
 
 
 weather = get_weather()
-display_avg_temps(weather)
+write_out_avg_temps(weather)
